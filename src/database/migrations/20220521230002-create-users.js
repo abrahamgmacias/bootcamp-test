ba87a631ca163a0ff11568e7bb73b58fe6492709
abrahamgmacias <abrahamgmacias@hotmail.com>
2022-05-21 17:39:08 -0600

    First migration

--- src/database/migrations/20220521230002-create-users.js
+++ src/database/migrations/20220521230002-create-users.js
@@ -1,29 +1,59 @@
 'use strict';
+
+const { DataTypes } = require("sequelize/types");
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      second_name: {
         type: Sequelize.STRING
       },
-      lastName: {
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      second_last_name: {
         type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      privileges: {
+        type: DataTypes.ARRAY(),
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+      payment_period_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      business_unit: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      on_leave: {
+        type: Sequelize.BOOLEAN
+      },
+      active: {
+        type: Sequelize.BOOLEAN
       }
     });
   },
